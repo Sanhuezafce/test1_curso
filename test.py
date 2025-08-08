6c4bf09eb4e10a6d028d1d7e51fed45427af0089
Felipe Sanhueza <sanhuezafce@gmail.com>
2025-08-08 18:45:23 -0400

    Actualizada logica de impuesto, profit, btc

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
 print("hola")
+
+""" 
+git add nombre_archivo.py  --> agregar 1 archivo para un commit
+git add * --> agregar todos los archivos para un commit
+
+git commit -m "mensaje"  --> orquestrar un commit 
+
+git push --> ejecuta cambios sobre el repo
+
+MEMORIA MUSCULAR
+git pull --> descargar cambios del repo remoto
+git status --> ver el estado de los archivos en el repo
+
+
+
+
+
+"""
